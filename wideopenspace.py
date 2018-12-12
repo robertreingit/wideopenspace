63d51efd90771fa832cf4f166c59697dbff697f5
robertreingit <git@robertrein.de>
2018-12-12 15:45:07 +0100

    Clean the house.

--- wideopenspace.py
+++ wideopenspace.py
@@ -130,18 +130,25 @@ def wideopen_transfer_func(x, min_distance = 4.0,
         Calculate the player's pitch control surface radius.
     
         Args:
-            x: numpy array of x-values
+            x: numpy array of x-values or scalar
             min_distance: minimun distance of player's pitch control radius
             max_distance: maximum distance of player's pitch control radius
             cut_off: distance where max_distance is reached
         Returns:
             player's control surface radius
     """
-    idx = x < cut_off
+
     alpha = np.log(max_distance - min_distance + 1.0 )/cut_off
-    y = np.full_like(x, 0.0)
-    y[np.invert(idx)] = max_distance
-    y[idx] = min_distance - 1.0 + np.exp(alpha*x[idx])
+    if np.isscalar(x):
+        if x < cut_off:
+            y = min_distance - 1.0 + np.exp(alpha * x)
+        else:
+            y = max_distance
+    else:
+        idx = x < cut_off
+        y = np.full_like(x, 0.0)
+        y[np.invert(idx)] = max_distance
+        y[idx] = min_distance - 1.0 + np.exp(alpha*x[idx])
     return y
 
 
@@ -161,36 +168,48 @@ def plot_player_influence(px, py, pz, mu_i_t, p1_v_f):
     """
     plt.contourf(px, py, pz, alpha=.8)
     plt.plot(mu_i_t.x, mu_i_t.y, 'ro', markersize=2)
-    plt.arrow(mu_i_t.x, mu_i_t.y, p1_v_f.x[frame], p1_v_f.y[frame], color='r')
+    plt.arrow(mu_i_t.x, mu_i_t.y, p1_v_f.x, p1_v_f.y, color='r')
 
 
-def calculate_influence(pos_xy, vel_xy, angle, S_rat, d_ball, grid):
+def calculate_influence(pos_xy, vel_xy, angle, D_i_t, grid_size = 0.5):
+    """Calculates the players influence according to Fernandez & Born.
+
+        Args:
+        Returns:
     """
-    """
+    # Eq. 16
     R_theta = generate_rotation_2x2_matrix(angle)
-    S = generate_direction_2x2_matrix(*vel_xy)
-    ## Distance to ball
     ## Transfer function
-    R_i_t =  wideopen_transfer_func(d_ball)
-    ## Generate S_i_t
+    R_i_t =  wideopen_transfer_func(D_i_t)
+    # Eq. 18
+    S_rat_i = np.sqrt(np.sum(vel_xy**2))/13.0**2
+    #print('{0:.2f} - {1:.2f} - {2:.2f} - {3:.2f}'.format(R_i_t, S_rat_i, vel_xy.x, vel_xy.y))
+    # Eq. 19
     S_i_t = np.diag([R_i_t + R_i_t * S_rat_i, R_i_t - R_i_t * S_rat_i])
-    ## Calculate covariance
+    # Eq. 20
     cov = R_theta.dot(S_i_t).dot(S_i_t).dot(R_theta.transpose())
-    ## calculate mu
-    p_i_t = p1.iloc[frame,:]
-    shat_i_t = p1_v_f.iloc[frame,:]
-    mu_i_t = p_i_t + shat_i_t * 0.5
-    # use some sort of ceil/floor mechanism to reduce the number of grid
-    # points needed to calculate the influence
-    px, py = np.mgrid[-52.5:52.6:0.1, -34:34.1:0.1]
+    # Eq. 21 
+    mu_i_t = pos_xy + vel_xy * 0.5
+
+    # determine necessary grid coordinates
+    grid_min = np.floor(mu_i_t - R_i_t)
+    grid_max = np.ceil(mu_i_t + R_i_t)
+    px, py = np.mgrid[slice(grid_min.x, grid_max.x, grid_size),
+            slice(grid_min.y, grid_max.y, grid_size)]
+
+    # generate structure for pdf-calculation.
     pos = np.empty(px.shape + (2,))
     pos[:, :, 0] = px; pos[:, :, 1] = py
     rv = multivariate_normal(mu_i_t, cov)
     pz = rv.pdf(pos)
+
     # zeroing out all points which are too far away from the player
-    # according to ball distancepz = rv.pdf(pos)
+    # according to ball distance
     radius_idx = np.sqrt((px - mu_i_t.x)**2 + (py - mu_i_t.y)**2) > R_i_t
-    pz[radius_idx] = np.nan
+    pz[radius_idx] = 0
+    # normalize influence
+    pz = pz / np.sum(pz)
+    return px, py, pz, mu_i_t 
 
 
 if __name__ == '__main__':
@@ -199,65 +218,22 @@ if __name__ == '__main__':
     ball.columns = ['x','y']
     plt.clf()
     plot_pitch(plt.gca())
+    frame = 90
     for i in [0,2,4,6,8,10,12,14,16,18,20]:
-        p1 = dat.iloc[:,slice(i,i+2)]
-        p1.columns = ['x','y']
-        #plt.clf()
-        #plot_pitch()
-        #plt.plot(p1.x, p1.y, 'y-')
-        p1_v = calculate_velocity(p1)
+        player = dat.iloc[:,slice(i,i+2)]
+        player.columns = ['x','y']
+        player_v = calculate_velocity(player)
         ## filter velocity
-        p1_v_f = filter_velocity(p1_v, 2.)
+        player_v_f = filter_velocity(player_v, 2.)
+        angle = calculate_angle_from_velocity(player_v_f)
+        D_i_t = distance_player_2_ball(player, ball)
 
-        """
-        plt.plot(p1_v, label=['velocity-x','velocity-y'])
-        plt.plot(p1_v_f,'r--', 
-                 label='velocity filtered')
-        plt.legend()
-        plt.show()
-        """
-        
-        ## calculate the directional angle
-        angle = calculate_angle_from_velocity(p1_v_f)
-        ## calculate the velocity magnitude
-        mag = calculate_velocity_magnitude(p1_v_f)
-        #plt.plot(angle)
-        frame = 20
-        R_theta = generate_rotation_2x2_matrix(angle[frame])
-        S = generate_direction_2x2_matrix(*p1_v_f.iloc[frame,:])
-        S_rat = calculate_S_rat_i(p1_v_f)
-        S_rat_i = S_rat[frame]
-        ## Distance to ball
-        D_i_t = distance_player_2_ball(p1, ball)
-        ## Transfer function
-        R_i_t =  wideopen_transfer_func(D_i_t[frame])
-        ## Generate S_i_t
-        S_i_t = np.diag([R_i_t + R_i_t * S_rat_i, R_i_t - R_i_t * S_rat_i])
-        ## Calculate covariance
-        cov = R_theta.dot(S_i_t).dot(S_i_t).dot(R_theta.transpose())
-        ## calculate mu
-        p_i_t = p1.iloc[frame,:]
-        shat_i_t = p1_v_f.iloc[frame,:]
-        mu_i_t = p_i_t + shat_i_t * 0.5
-        px, py = np.mgrid[-52.5:52.6:0.1, -34:34.1:0.1]
-        pos = np.empty(px.shape + (2,))
-        pos[:, :, 0] = px; pos[:, :, 1] = py
-        rv = multivariate_normal(mu_i_t, cov)
-        pz = rv.pdf(pos)
-        # zeroing out all points which are too far away from the player
-        # according to ball distancepz = rv.pdf(pos)
-        radius_idx = np.sqrt((px - mu_i_t.x)**2 + (py - mu_i_t.y)**2) > R_i_t
-        pz[radius_idx] = np.nan
-        # 
+        px, py, pz, mu_i_t = calculate_influence(player.iloc[frame],
+                player_v_f.iloc[frame], angle[frame], D_i_t[frame])
+    
+        plot_player_influence(px, py, pz, mu_i_t, player_v_f.iloc[frame])
 
-        
-        """
-        plt.contourf(px, py, pz)
-        plt.plot(mu_i_t.x, mu_i_t.y, 'ro', markersize=2)
-        plt.arrow(mu_i_t.x, mu_i_t.y, p1_v_f.x[frame], p1_v_f.y[frame], color='r')
-        """
-        plot_player_influence(px, py, pz, mu_i_t, p1_v_f)
-
+    plt.plot(ball.x[frame], ball.y[frame], 'ro', markersize=5)
     plt.gca().set_aspect('equal', adjustable='box')
     plt.show()
     
